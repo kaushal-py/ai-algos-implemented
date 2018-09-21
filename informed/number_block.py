17c7e0260328cd43c8dd32c02ef53c024637675e
kb-studios <2015kaushal.bhogale@ves.ac.in>
2018-09-21 22:54:02 +0530

    Add Heuristic function

--- informed/number_block.py
+++ informed/number_block.py
@@ -9,6 +9,10 @@ UP = 8
 DOWN = 2
 LEFT = 4
 RIGHT = 6
+DIRECTIONS = [UP, DOWN, LEFT, RIGHT]
+
+# A max int value for heuristic, i.e unreachable state.
+INFINITY = WORLD_SIZE*WORLD_SIZE + 1
 
 for i in range(WORLD_SIZE*WORLD_SIZE):
     WORLD[i] = random.randint(1, 3)
@@ -19,6 +23,11 @@ WORLD = [WORLD[i:i+WORLD_SIZE] for i in range(0, len(WORLD), WORLD_SIZE)]
 
 start_location = [0, 0]
 
+# instead of calculating the value dynamically, which is asymtotically slower.
+H_MEM = [[INFINITY for _ in range(WORLD_SIZE)] for _ in range(WORLD_SIZE)]
+H_MEM[WORLD_SIZE-1][WORLD_SIZE-1] = 0
+
+print(H_MEM)
 
 def print_grid(location):
 
@@ -83,4 +92,33 @@ def simulate():
             print("Goal reached!")
             break
 
-simulate()
+
+def calcDistanceToGoal():
+    
+    for i in range(WORLD_SIZE):
+        for j in range(WORLD_SIZE):
+            
+            for k in range(WORLD_SIZE):
+                for l in range(WORLD_SIZE):
+                    
+                    if H_MEM[k][l] == INFINITY:
+                        
+                        for direction in DIRECTIONS:
+
+                            neigh_location = move([k,l], direction)
+
+                            if neigh_location != [k,l]:
+                                neigh_heuristic = H_MEM[neigh_location[0]][neigh_location[1]]
+
+                                if neigh_heuristic != INFINITY:
+                                    H_MEM[k][l] = neigh_heuristic+1
+    
+    if H_MEM[0][0] < INFINITY:
+        return True
+    else:
+        return False
+
+print(calcDistanceToGoal())
+
+print(H_MEM)
+#simulate()
