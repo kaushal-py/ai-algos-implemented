9880a41385f0be2573d31ca919204661663f8187
kb-studios <2015kaushal.bhogale@ves.ac.in>
2018-10-02 17:03:50 +0530

    a really bad heuristic

--- game-playing/connect-4.py
+++ game-playing/connect-4.py
@@ -9,6 +9,10 @@ PLAYERS = ("R", "B")
 
 LOOKUP_DEPTH = 4
 
+# set 1 if you want to move first, 
+# else 0 if you want pc to make move first.
+FIRST_MOVE = 1
+
 start_state = [ ['E' for _ in range(COLUMNS)] for _ in range(ROWS)]
 
 
@@ -107,8 +111,7 @@ def max_connect_val(state, pos, player):
     return max(count_down, count_diag_left, count_diag_right, count_left+count_right-1)
 
 
-
-def simulate():
+def simulate_2_player():
 
     os.system('clear')
 
@@ -133,7 +136,7 @@ def simulate():
         turn = 1-turn
         
 
-def utility(state, player):
+def utility_basic(state, player):
 
     utility = 0
 
@@ -141,61 +144,171 @@ def utility(state, player):
         for j in range(COLUMNS):
             utility = max(utility, max_connect_val(state, (i,j), player))
 
-    return utility 
+    return utility
+
+
+def utility(state, player):
+
+    if PLAYERS[0] == player:
+        opponent = PLAYERS[1]
+    else:
+        opponent = PLAYERS[0]
+    
+    empty_spaces_player = 0
+    empty_spaces_opponent = 0
+
+    if utility_basic(state, player) >= 4:
+        return 100
+    if utility_basic(state, opponent) >= 4:
+        return -100
+
+    for i in range(ROWS):
+        for j in range(COLUMNS):
+
+            if state[i][j] != 'E':
+
+                count_spaces = 0
+
+                # top
+                if i-1 >=0 and state[i-1][j] == 'E':
+                    count_spaces += 1
+
+                # right
+                try:
+                    if j+1 < COLUMNS and state[i][j+1] == 'E' and state[i+1][j+1] != 'E':
+                        count_spaces += 1
+                except:
+                    count_spaces += 1
+                
+                # left
+                try:
+                    if j-1 >= 0 and state[i][j-1] == 'E' and state[i+1][j-1] != 'E':
+                        count_spaces += 1
+                except:
+                    count_spaces += 1
+                # top right
+                #if i-1 >= 0 and j+1 < COLUMNS and state[i-1][j+1] == 'E'
+
+                if state[i][j] == player:
+                    empty_spaces_player += count_spaces
+                else:
+                    empty_spaces_opponent += count_spaces
+    
+    return empty_spaces_player - empty_spaces_opponent
 
 
 
-def maxValue(state, alpha, beta, depth, player):
+
+
+def max_value(state, alpha, beta, depth, player, column):
 
     if depth == 0:
-        return utility(state, player)
+        return {
+            "value" : utility(state, player),
+            "column" : column
+        }
     
 
-    v = -INFINITY
+    v = {
+        "value" : -INFINITY,
+        "column" : -1
+    }
 
     for col in range(COLUMNS):
         neighbour, _ = move(state, col, player)
-        v = max(v, min_value(neighbour, alpha, beta, depth-1, player))
 
-        if v >= beta:
+        max_val = min_value(neighbour, alpha, beta, depth-1, player, col)
+        
+        if v["value"] < max_val["value"]:
+            v["value"] = max_val["value"]
+            v["column"] = col
+
+        if v["value"] >= beta:
             return v
         
-        alpha = max(alpha, v)
+        alpha = max(alpha, v["value"])
     
+    #print(v)
     return v
 
 
 
-def min_value(state, alpha, beta, depth, player):
+def min_value(state, alpha, beta, depth, player, column):
 
     if depth == 0:
-        return utility(state, player)
+        return {
+            "value" : utility(state, player),
+            "column" : column
+        }
     
 
-    v = INFINITY
+    v = {
+        "value" : INFINITY,
+        "column" : -1
+    }
 
     for col in range(COLUMNS):
         neighbour, _ = move(state, col, player)
-        v = min(v, max_value(neighbour, alpha, beta, depth-1, player))
+        
+        min_val = max_value(neighbour, alpha, beta, depth-1, player, col)
+        
+        if v["value"] > min_val["value"]:
+            v["value"] = min_val["value"]
+            v["column"] = col
 
-        if v <= alpha:
+        if v["value"] <= alpha:
             return v
         
-        alpha = max(alpha, v)
+        alpha = max(alpha, v["value"])
     
+    #print(v)
     return v
 
 
 
-def alpha-beta-search(state, depth, player):
+def alpha_beta_search(state, depth, player):
 
-    v = max_value(state, INFINITY, -INFINITY, depth, player)
+    v = max_value(state, -INFINITY, INFINITY, depth, player, -1)
 
+    return v["column"]
+
+
+def simulate_VS_pc():
+
+    os.system('clear')
+
+    curr_state = start_state
+
+    turn = FIRST_MOVE
+
+    while(True):
+
+        print_world(curr_state)
+
+
+        if turn == 1:
+            col = int(input("Enter the column: "))
+        else:
+            col = alpha_beta_search(curr_state, 3, PLAYERS[turn])
+
+        os.system('clear')
+        next_state, connect_val = move(curr_state, col, PLAYERS[turn])
+
+        if next_state == curr_state:
+            continue
+        curr_state=next_state
+
+        if connect_val == 4:
+            print_world(curr_state)
+            print(PLAYERS[turn], "won!")
+            break
+        
+        turn = 1-turn
 
 
 def main():
     
-    simulate()
+    simulate_VS_pc()
 
 if __name__ == '__main__':
     main()
